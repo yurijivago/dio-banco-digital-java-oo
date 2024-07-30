b8f6116526ba902fc6aca701193855d3489d05ec
Yuri Jivago <yurijivagoads@gmail.com>
2024-07-29 22:48:38 -0300

    Classe Banco

--- dio-banco-digital/src/models/Banco.java
+++ dio-banco-digital/src/models/Banco.java
@@ -3,5 +3,22 @@ package models;
 import java.util.List;
 
 public class Banco {
-    private String nomeBanco;
+    private String nome;
+    private List<Conta> contas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
+    }
 }
