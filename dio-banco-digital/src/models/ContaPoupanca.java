f617fe06e78f44347ccbbc74d88947cde2f30bfb
Yuri Jivago <yurijivagoads@gmail.com>
2024-07-29 22:48:02 -0300

    Contas: correte e poupança

--- dio-banco-digital/src/models/ContaPoupanca.java
+++ dio-banco-digital/src/models/ContaPoupanca.java
@@ -1,7 +1,13 @@
 package models;
 
-public class ContaPoupanca {
-    private int agencia;
-    private int numero;
-    private double saldo;
+public class ContaPoupanca extends Conta{
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Poupança ===");
+        super.imprimirInfosComuns();
+    }
 }
