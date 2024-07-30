f1e26df2bd4420d4ada49cfc08f701f12c64a327
Yuri Jivago <yurijivagoads@gmail.com>
2024-07-29 22:44:36 -0300

    Classe Conta

--- dio-banco-digital/src/models/Conta.java
+++ dio-banco-digital/src/models/Conta.java
@@ -8,4 +8,49 @@ public abstract class Conta implements IConta{
     protected int numero;
     protected double saldo;
 
+    private Cliente cliente;
+
+    public Conta() {}
+
+    public Conta(int agencia, int numero, double saldo, Cliente cliente) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = saldo;
+        this.cliente = cliente;
+    }
+
+    @Override
+    public void  sacar(double valor){
+        saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    @Override
+    public void transferir(double valor, IConta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    protected void imprimirInfosComuns() {
+        System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+        System.out.println(String.format("Agencia: %d", this.agencia));
+        System.out.println(String.format("Numero: %d", this.numero));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
+    }
 }
